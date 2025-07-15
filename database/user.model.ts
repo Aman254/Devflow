c43d49f202ae54816c40e42092e750e59a4fb0cf
Aman <aman.mishra.2005@gmail.com>
2025-07-15 13:15:02 +0530

    Implemented a User Model

--- database/user.model.ts
+++ database/user.model.ts
@@ -1,1 +1,28 @@
-const UserSchema = new Schema();
+import { model, models, Schema } from "mongoose";
+
+export interface IUser {
+  name: string;
+  username: string;
+  bio: string;
+  image: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, requires: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String },
+    image: { type: String, required: true },
+    portfolio: { type: String },
+    reputation: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const User = models?.user || model<IUser>("User", UserSchema);
+
+export default User;
